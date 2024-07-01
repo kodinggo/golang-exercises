23b70c7c20217b9d27221cc641a9a35a71504c9c
Reza Indra <edu.rezaindra@gmail.com>
2024-07-01 10:04:47 +0700

    add management handler

--- echo-middleware/main.go
+++ echo-middleware/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+type ctxKey string
+
+const key ctxKey = "key"
 
 func main() {
 	e := echo.New()
@@ -15,6 +21,11 @@ func main() {
 
 	mid1 := func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			// Mengirimkan data dg context dari middleware
+			ctx := context.WithValue(c.Request().Context(), key, "value")
+			req := c.Request().WithContext(ctx)
+			c.SetRequest(req)
+
 			log.Println("before enter handler (middlware 1)")
 			err := next(c)
 			log.Println("after enter handler (middlware 1)")
@@ -33,6 +44,9 @@ func main() {
 
 	e.GET("/", func(c echo.Context) error {
 		log.Println("entering handler")
+		// Mendapatkan data dari context middleware
+		fmt.Println(c.Request().Context().Value("key"))
+
 		return c.String(http.StatusOK, "hi")
 	}, mid1, mid2)
 
