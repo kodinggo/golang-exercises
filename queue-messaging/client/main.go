3744ad3cd0524990c1bf1817c9544cf2f875c7eb
Reza Indra <edu.rezaindra@gmail.com>
2024-07-31 11:47:54 +0700

    implement asynq queue

--- queue-messaging/client/main.go
+++ queue-messaging/client/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"queue-messaging/task"
+	"sync"
 
 	"github.com/hibiken/asynq"
 )
@@ -16,16 +19,45 @@ func main() {
 	// Init client
 	client := asynq.NewClient(redisOpt)
 
+	var wg sync.WaitGroup
+	wg.Add(20)
+
+	for i := 0; i < 20; i++ {
+		go sendEmail(&wg, client, i)
+	}
+
+	wg.Wait()
+
+	log.Println("success publish an event")
+}
+
+func sendEmail(wg *sync.WaitGroup, client *asynq.Client, i int) {
+	defer wg.Done()
+
 	// prepare payloao
 	payload, _ := json.Marshal(task.SendEmail{
 		From:    "foo@gmail.com",
 		To:      "bar@gmail.com",
-		Subject: "Testing",
+		Subject: fmt.Sprintf("Testing %d", i),
 		Content: "Testing",
 	})
 
 	// init task
 	task := asynq.NewTask(task.TaskSendEmail, payload)
+
+	// enqueue (publish)
+	client.Enqueue(task, asynq.Queue("critical"), asynq.MaxRetry(3))
+}
+
+func uploadFile(client *asynq.Client) {
+	// prepare payloao
+	payload, _ := json.Marshal(task.UplaodFile{
+		Origin:      "/path/to/origin/1.jpg",
+		Destination: "/path/to/destination/1.jpg",
+	})
+
+	// init task
+	task := asynq.NewTask(task.TaskUploadFile, payload)
 
 	// enqueue (publish)
 	client.Enqueue(task)
--- queue-messaging/server/main.go
+++ queue-messaging/server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"log"
 	"queue-messaging/task"
+	"time"
 
 	"github.com/hibiken/asynq"
 )
@@ -16,13 +19,32 @@ func main() {
 
 	// init asynq server
 	svr := asynq.NewServer(redisOpt, asynq.Config{
-		Concurrency: 10,
+		Concurrency: 1,
+		Queues: map[string]int{
+			"critical": 6,
+			"default":  3,
+			"low":      1,
+		},
+		ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+			// log.Println("task ID", task.ResultWriter().TaskID())
+			log.Println("task payload", string(task.Payload()))
+		}),
 	})
 
 	mux := asynq.NewServeMux()
 
 	mux.HandleFunc(task.TaskSendEmail, func(ctx context.Context, t *asynq.Task) error {
-		log.Println("Sending email....")
+		time.Sleep(1 * time.Second)
+
+		// Mendapatkan paylaod
+		var payload task.SendEmail
+		_ = json.Unmarshal(t.Payload(), &payload)
+
+		if payload.Subject == "Testing 10" {
+			return errors.New("error")
+		}
+
+		log.Printf("Sending email, subject %s....", payload.Subject)
 		return nil
 	})
 
