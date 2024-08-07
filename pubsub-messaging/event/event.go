41e8cf2c6853fb3fcb28aa0a5fe3bafd6ea699fa
Reza Indra <edu.rezaindra@gmail.com>
2024-08-07 10:23:00 +0700

    add another event

--- pubsub-messaging/event/event.go
+++ pubsub-messaging/event/event.go
@@ -3,7 +3,9 @@ package event
 const (
 	ProductsTopic = "PRODUCTS"
 
-	ProductsEvent      = "PRODUCTS.*"
+	ProductsEvent = "PRODUCTS.*"
+	// ProductsEvent      = "PRODUCTS.>"
+	// CreateProductCategoryAEvent = "PRODUCTS.category-a.create"
 	CreateProductEvent = "PRODUCTS.create"
 	UpdateProductEvent = "PRODUCTS.update"
 	DeleteProductEvent = "PRODUCTS.delete"
